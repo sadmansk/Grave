e18a202c7472eab64813a4bde0ffc94a36663e23
Sadman Kazi <sadmansazidk@gmail.com>
2014-10-04 17:42:36 -0400

    Added all the files

---
+++ Assets/_scripts/camera_control.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class camera_control : MonoBehaviour {
+	public Transform player;
+	private Vector3 offset;
+
+	void Start() {
+		offset = transform.position;
+	}
+	
+	// Update is called once per frame
+	void LateUpdate () {
+		transform.position = player.position + offset;
+	}
+}
---
+++ Assets/_scripts/control_plane.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class control_plane : MonoBehaviour {
+	//declares the speed variables
+	float speed = 1.0f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		float xRotate = Input.GetAxis ("Vertical") * speed;
+		float zRotate = Input.GetAxis ("Horizontal") * speed;
+
+		transform.Rotate(xRotate, 0, zRotate);
+	}
+}
